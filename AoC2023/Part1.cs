47a057e8e66a96e0270e6051ebf0af79c97df211
Doug Luxem <doug@luxem.org>
2023-11-30 22:24:32 -0600

    init

---
+++ AoC2023/Part1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+internal class Part1
+{
+    public static void Run()
+    {
+        var inputFile = File.OpenText("input.txt");
+
+
+        while (!inputFile.EndOfStream)
+        {
+            var line = inputFile.ReadLine();
+            
+
+        }
+
+
+        Console.WriteLine("Part1 Answer");
+        
+
+    }
+
+}
+

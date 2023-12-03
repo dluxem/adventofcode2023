724e1e2bfe4039e36f03eb975b4979fce3ef1169
Doug Luxem <doug@luxem.org>
2023-12-03 00:36:38 -0600

    Day 3

---
+++ Day03/Part1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+internal class Part1
+{
+    static List<string> schematic = new List<string>();
+
+    public static void Run()
+    {
+        var inputFile = File.OpenText("input.txt");
+        var total = 0;
+
+        while (!inputFile.EndOfStream)
+        {
+            var line = inputFile.ReadLine();
+            schematic.Add(line);
+        }
+
+        var lineCount = 0;
+        foreach (var line in schematic)
+        {
+            var lineLength = line.Length;
+            var matches = Regex.Match(line, "(\\d+)");
+            while (matches.Success)
+            {
+                var result = checkSymbols(matches.Index, (matches.Index + matches.Length), lineCount, lineLength);
+                if (result)
+                {
+                    total += Convert.ToInt32(matches.Value);
+                }
+                matches = matches.NextMatch();
+            }
+            lineCount++;
+        }
+
+
+        Console.WriteLine(total);
+        
+
+    }
+
+    private static bool checkSymbols(int start, int end, int lineNumber, int maxLineLength)
+    {
+        if (start > 0) { start = start - 1; }
+        if (end < maxLineLength-1) { end = end + 1; }
+
+        var checkString = "";
+        if (lineNumber > 0) // n-1
+        {
+            checkString = checkString + schematic[lineNumber - 1].Substring(start, end - start);
+        }
+        checkString = checkString + schematic[lineNumber].Substring(start, end - start);
+        if (lineNumber < schematic.Count - 1)
+        {
+            checkString = checkString + schematic[lineNumber + 1].Substring(start, end - start);
+        }
+
+        checkString = Regex.Replace(checkString, "[\\.\\d]*", "");
+        if (checkString.Length > 0) { return  true; }
+        else { return false; }
+    }
+
+}
+
---
+++ Day03/Part2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+internal class Part2
+{
+    static List<string> schematic = new List<string>();
+
+    public static void Run()
+    {
+        var inputFile = File.OpenText("input.txt");
+        var total = 0;
+
+        while (!inputFile.EndOfStream)
+        {
+            var line = inputFile.ReadLine();
+            schematic.Add(line);
+        }
+
+        var lineCount = 0;
+        foreach (var line in schematic)
+        {
+            var lineLength = line.Length;
+            var matches = Regex.Match(line, "(\\*)");
+            while (matches.Success)
+            {
+                total += checkGears(matches.Index, lineCount);
+                matches = matches.NextMatch();
+            }
+            lineCount++;
+        }
+
+
+        Console.WriteLine(total);
+
+
+    }
+
+    private static int checkGears(int index, int lineNumber)
+    {
+        List<int> nearby = new List<int>();
+
+        //above
+        if (lineNumber > 0) {
+            var m = Regex.Match(schematic[lineNumber-1],"(\\d+)");
+            while (m.Success)
+            {
+                if (index >= m.Index-1 && index <= m.Index+m.Value.Length)
+                {
+                    nearby.Add(Convert.ToInt32(m.Value));
+                }
+                m = m.NextMatch();
+            }
+        }
+
+        //below
+        if (lineNumber < schematic.Count-1)
+        {
+            var m = Regex.Match(schematic[lineNumber + 1], "(\\d+)");
+            while (m.Success)
+            {
+                if (index >= m.Index - 1 && index <= m.Index + m.Value.Length)
+                {
+                    nearby.Add(Convert.ToInt32(m.Value));
+                }
+                m = m.NextMatch();
+            }
+        }
+
+        // same line
+        var mSame = Regex.Match(schematic[lineNumber], "(\\d+)");
+        while (mSame.Success)
+        {
+            if (index == mSame.Index - 1 || index == mSame.Index + mSame.Value.Length)
+            {
+                nearby.Add(Convert.ToInt32(mSame.Value));
+            }
+            mSame = mSame.NextMatch();
+        }
+
+        var results = nearby.Where<int>(i => i >= 0).ToList();
+        if (results.Count() != 2 ) {
+            return 0;
+        }
+        else
+        {
+            Console.WriteLine("Line: {0}, Index: {1}, Gears: {2}, {3}", lineNumber, index, results[0], results[1]);
+            return results[0] * results[1];
+        }
+
+    }
+
+}
+

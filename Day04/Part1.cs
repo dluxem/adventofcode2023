b0aa2dfe8ee2a93bd80a1acfb3cc8001cfb59459
Doug Luxem <doug@luxem.org>
2023-12-06 22:19:47 -0600

    day 04

---
+++ Day04/Part1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+internal class Part1
+{
+    public static void Run()
+    {
+        var inputFile = File.OpenText("input.txt");
+
+        var total = 0;
+        while (!inputFile.EndOfStream)
+        {
+            var line = inputFile.ReadLine();
+            var parts = line.Split(new char[] { ':', '|' },StringSplitOptions.RemoveEmptyEntries);
+            string[] winning = parts[1].Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            var correct = 0;
+            foreach (var item in parts[2].Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (winning.Contains(item)) { correct++;  }
+            }
+            if (correct > 0) { total += Power(2, correct-1); }
+
+        }
+
+
+        Console.WriteLine(total);
+        
+
+    }
+
+    public static int Power(int x, int power)
+    {
+        int result = 1;
+        for (int i = 0; i < power; i++)
+        {
+            result *= x;
+        }
+        return result;
+    }
+
+}
+
---
+++ Day04/Part2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc;
+
+internal class Part2
+{
+    private static Dictionary<int, int> cardWins = new Dictionary<int, int>();
+    private static Dictionary<int, int> cardsToProcess = new Dictionary<int, int>();
+
+    public static void Run()
+    {
+        var inputFile = File.OpenText("input.txt");
+        var cardNumber = 0;
+        while (!inputFile.EndOfStream)
+        {
+            var line = inputFile.ReadLine();
+            cardNumber++;
+
+            var parts = line.Split(new char[] { ':', '|' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] winning = parts[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var correct = 0;
+            foreach (var item in parts[2].Split(" ", StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (winning.Contains(item)) { correct++; }
+            }
+            cardWins.Add(cardNumber, correct);
+        }
+
+        for (int i = cardNumber; i >0; i--)
+        {
+            cardsToProcess.Add(i, ProcessCards(i));
+        }
+
+        var total = 0;
+        foreach (var card in cardsToProcess)
+        {
+            total += card.Value;
+        }
+        Console.WriteLine(total);
+
+    }
+
+    private static int ProcessCards(int q)
+    {
+        var count=1;
+        var cardNumber = q;
+        var wins = cardWins[cardNumber];
+        for (int i = cardNumber+1; i < cardNumber+1+wins ; i++)
+        {
+            if (cardsToProcess[i] != 0) { count += cardsToProcess[i]; }
+        }
+        return count;
+
+    }
+
+}
+
